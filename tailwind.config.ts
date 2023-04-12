942f5553ad14fb6fbcc1ac26b1d72bd2aa64a047
bvpav <bvpav@tutanota.com>
2023-04-12 18:14:39 +0300

    styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,18 @@
 import { type Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)", ...fontFamily.sans],
+        origintech: ["var(--font-origintech)", ...fontFamily.sans],
+      },
+      colors: {
+        black: "rgb(20, 20, 32)",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
