6c46a52e53e3b19c1c9438a89af61de2bb137298
bvpav <78971592+bvpav@users.noreply.github.com>
2023-07-14 19:43:34 +0300

    Switched tRPC runtime to edge

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,38 +1,47 @@
-import { createNextApiHandler } from "@trpc/server/adapters/next";
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 
+import { type NextRequest } from "next/server";
 import { env } from "~/env.mjs";
-import { createTRPCContext } from "~/server/api/trpc";
 import { appRouter } from "~/server/api/root";
+import { createTRPCContext } from "~/server/api/trpc";
 
 // export API handler
-export default createNextApiHandler({
-  router: appRouter,
-  createContext: createTRPCContext,
-  onError:
-    env.NODE_ENV === "development"
-      ? ({ path, error }) => {
-          console.error(
-            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
-          );
-        }
-      : undefined,
-  responseMeta({ ctx, paths, errors, type }) {
-    const allPublic = paths && paths.every((path) => path.includes("videos"));
-    // checking that no procedures errored
-    const allOk = errors.length === 0;
-    // checking we're doing a query request
-    const isQuery = type === "query";
-    // FIXME: the docs suggest to also check for ctx?.res, but we're not passing it, dk if we should
-    if (allPublic && allOk && isQuery) {
-      // cache request for 1 day + revalidate once every 2 minutes
-      const ONE_DAY_IN_SECONDS = 60 * 60 * 24;
-      const TWO_MINUTES_IN_SECONDS = 60 * 2;
-      return {
-        headers: {
-          "cache-control": `s-maxage=${TWO_MINUTES_IN_SECONDS}, stale-while-revalidate=${ONE_DAY_IN_SECONDS}`,
-        },
-      };
-    }
-    return {};
-  },
-});
+export default function handler(req: NextRequest) {
+  return fetchRequestHandler({
+    req,
+    endpoint: "/api/trpc",
+    router: appRouter,
+    createContext: createTRPCContext,
+    onError:
+      env.NODE_ENV === "development"
+        ? ({ path, error }) => {
+            console.error(
+              `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
+            );
+          }
+        : undefined,
+    responseMeta({ paths, errors, type }) {
+      const allPublic = paths && paths.every((path) => path.includes("videos"));
+      // checking that no procedures errored
+      const allOk = errors.length === 0;
+      // checking we're doing a query request
+      const isQuery = type === "query";
+      // FIXME: the docs suggest to also check for ctx?.res, but we're not passing it, dk if we should
+      if (allPublic && allOk && isQuery) {
+        // cache request for 1 day + revalidate once every 2 minutes
+        const ONE_DAY_IN_SECONDS = 60 * 60 * 24;
+        const TWO_MINUTES_IN_SECONDS = 60 * 2;
+        return {
+          headers: {
+            "cache-control": `s-maxage=${TWO_MINUTES_IN_SECONDS}, stale-while-revalidate=${ONE_DAY_IN_SECONDS}`,
+          },
+        };
+      }
+      return {};
+    },
+  });
+}
+
+export const config = {
+  runtime: "edge",
+};
