f7369423f97b68062ee0140b73ebc638b891b9cd
bvpav <78971592+bvpav@users.noreply.github.com>
2023-07-14 18:43:41 +0300

    Added tRPC response caching

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -12,8 +12,27 @@ export default createNextApiHandler({
     env.NODE_ENV === "development"
       ? ({ path, error }) => {
           console.error(
-            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
+            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
           );
         }
       : undefined,
+  responseMeta({ ctx, paths, errors, type }) {
+    const allPublic = paths && paths.every((path) => path.includes("videos"));
+    // checking that no procedures errored
+    const allOk = errors.length === 0;
+    // checking we're doing a query request
+    const isQuery = type === "query";
+    // FIXME: the docs suggest to also check for ctx?.res, but we're not passing it, dk if we should
+    if (allPublic && allOk && isQuery) {
+      // cache request for 1 day + revalidate once every 2 minutes
+      const ONE_DAY_IN_SECONDS = 60 * 60 * 24;
+      const TWO_MINUTES_IN_SECONDS = 60 * 2;
+      return {
+        headers: {
+          "cache-control": `s-maxage=${TWO_MINUTES_IN_SECONDS}, stale-while-revalidate=${ONE_DAY_IN_SECONDS}`,
+        },
+      };
+    }
+    return {};
+  },
 });
